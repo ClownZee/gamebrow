d4ed358c2fa835d609ba9798bf263726e131d3c3
Agil <77065393+ClownZee@users.noreply.github.com>
2025-01-31 00:00:18 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GameList from "./components/GameList"; // Pastikan path sesuai
 import GameDetail from "./components/GameDetail";
-import OnlineGameList from "./components/OnlineGameList"; // Pastikan path sesuai
+import OnlineGameList from "./components/OnlineGameList";
+import UpdateGame from "./components/UpdateGame";  // Pastikan path sesuai
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<GameList />} /> {/* Tambahkan daftar game di halaman utama */}
         <Route path="/game/:slug" element={<GameDetail />} /> {/* Rute detail game */}
         <Route path="/onlinegames" element={<OnlineGameList />} />
+        <Route path="/updategame" element={<UpdateGame />} />
       </Routes>
     </Router>
   );
